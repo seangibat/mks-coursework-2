7d9db390ecea24d69cfc2584d82a158a49b9e6ae
Sean Gibat <sgibat@gmail.com>
2015-01-14 11:13:45 -0800

    implemented orderBy and list and grid views

--- amazon/app/assets/javascripts/controllers/ItemsController.js
+++ amazon/app/assets/javascripts/controllers/ItemsController.js
@@ -1,4 +1,10 @@
 app.controller('ItemsController', ['$scope', 'Items', 'Cart', function($scope, Items, Cart){
   $scope.items = Items;
   $scope.cart = Cart;
+
+  $scope.sortOptions = ['name', 'price', 'quantity'];
+  $scope.layoutOptions = ['grid', 'list'];
+
+  $scope.selectedOrder = $scope.sortOptions[0];
+  $scope.layout = $scope.layoutOptions[0];
 }]);
